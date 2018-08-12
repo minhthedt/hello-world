d5b7adc7cdba4edbf9668fa3bb68c35e9862f387
minhthedt <minhthedt@gmail.com>
2018-08-12 23:55:53 +0700

    problem 1

---
+++ Jam/Jam/include/Common.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <string>
+#include "Utils.h"
+
+#define DATA_PROBLEM_2018_1_101 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-1.in"
+#define DATA_PROBLEM_2018_1_110 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-10.in"
+#define DATA_PROBLEM_2018_1_111 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-11.in"
+#define DATA_PROBLEM_2018_1_112 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-12.in"
+#define DATA_PROBLEM_2018_1_113 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-13.in"
+#define DATA_PROBLEM_2018_1_114 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-14.in"
+#define DATA_PROBLEM_2018_1_115 "D:\\Training\\github\\data\\2018\\1st_ProblemA_subtask1\\data-15.in"
