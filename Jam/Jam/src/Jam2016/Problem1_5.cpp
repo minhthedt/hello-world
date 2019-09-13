40971acdc91c992f4dd84cba42dc9dd5dbb0f95a
minhthedt <minhthedt@gmail.com>
2019-09-13 21:14:54 +0700

    2016_1_5

--- Jam/Jam/src/Jam2016/Problem1_5.cpp
+++ Jam/Jam/src/Jam2016/Problem1_5.cpp
@@ -5,120 +5,97 @@
 #include <chrono>
 #include <math.h>
 using namespace std;
-
+//[2019-09-13] chưa hiểu rõ lời giải: kiểu quy hoạch động + phân chia nhiều trường hợp
 namespace Jam2016
 {
-#define UINT8 unsigned char
-#define INT16 signed short 
-#define UINT16 unsigned short 
-#define UINT32 unsigned int
-#define INT32  signed int
-#define INT64 signed long long
-#define UINT64 unsigned long long
-#define PI  3.1415926535897932384626344
-
-    UINT64 getMax(INT32 N, UINT16 *P, UINT16 *S, UINT64 *sum)
+#define MAX_N 2000
+#define max(a, b) ((a > b) ? a : b)
+    int power[MAX_N];
+    int female[MAX_N];
+    int N;
+    int chk_cal[MAX_N + 1][MAX_N];
+    int next_fe[MAX_N];
+    inline int mul_p(int a, int b)
     {
-       
-        sum[0] = 0;//have only 0 snake
-        sum[1] = 0;//have only 1 snake
-        //root -> N -> 1 -> root -1
-        int ii = 0 , jj = 0;
-        for (int i = 2; i <= N; i++)
-        {
-            if (sum[i] == 0) sum[i] = sum[i - 1];
-          
-            for (int j = i - 1; j >= 1; j--)
-            {                
-                //tính trường hợp snake tham gia phát điện
-                if (S[j] != S[i])//khác giới
-                {
-                    UINT64 temp = P[i] * P[j] + sum[j - 1];
-                    //sum[i] = sum[i] > temp ? sum[i] : temp;
-                    if (sum[i] < temp)
-                    {
-                        sum[i] = temp;
-                        ii = i;
-                        jj = j;
+        if (female[a] != female[b])
+            return power[a] * power[b];
+        return 0;
+    }
+    int find_max_power(void)
+    {
+        int i, k;
+        int st, ed, len;
+        int p;
+        int max_p = 0;
+
+        for (i = 0, k = i; i < N; i++) {
+            if (female[i])
+                while (k <= i)
+                    next_fe[k++] = i;
+        }
+        while (k < N)
+            next_fe[k++] = MAX_N + 1;
+
+        for (i = 0; i < N - 1; i++) {
+            chk_cal[i][i + 1] = mul_p(i, i + 1);
+        }
+        for (len = 3; len <= N; len++) {
+            for (st = 0; st < N - len + 1; st++) {
+                ed = st + len - 1;
+                int n_st = next_fe[st];
+                int n_ed = next_fe[ed];
+
+                max_p = max(chk_cal[st][ed - 1], chk_cal[st + 1][ed]);
+                p = mul_p(st, ed) + chk_cal[st + 1][ed - 1];
+                max_p = max(p, max_p);
+                if (female[st] == 0) {
+                    for (k = n_st; k < n_ed; k = next_fe[k + 1]) {
+                        p = chk_cal[st][k] + chk_cal[k + 1][ed];
+                        max_p = max(p, max_p);
                     }
                 }
-
+                else if (female[st] == 1 && female[ed] == 0) {
+                    for (k = n_st; k < n_ed; k = next_fe[k + 1]) {
+                        p = chk_cal[st][k - 1] + chk_cal[k][ed];
+                        max_p = max(p, max_p);
+                    }
+                }
+                else { /* (female[st] == 1 && female[ed] == 1) */
+                    for (k = st + 1; k < ed - 1; k++) {
+                        p = chk_cal[st][k] + chk_cal[k + 1][ed];
+                        max_p = max(p, max_p);
+                    }
+                }
+                chk_cal[st][ed] = max_p;
             }
-            //printf("[%d] ii = %d jj = %d ----> %d\n",i, ii, jj, sum[i]);
         }
-        //printf("%d\n", sum[N]);
-        return sum[N];
+        return max_p;
     }
-
-    void ShiftLeft(UINT16 *dt, UINT32 N)
-    {
-        //dt size N + 1
-        UINT16 bk = dt[1];
-        for (int i = 1; i <= N-1; i++)
-        {
-            dt[i] = dt[i + 1];
-        }
-        dt[N] = bk;
-    };
-
-    UINT64 getMaximumAmount(INT32 N, UINT16 *P, UINT16 *S, UINT64 *sum)
-    {
-        UINT64 max = 0;
-        INT32 loop = N;
-        while (loop > 0)
-        {
-            memset(sum, 0, (N + 1) * sizeof(UINT64));
-            UINT64 temp = getMax(N, P, S, sum);
-            max = max > temp ? max : temp;
-            //shift left
-            ShiftLeft(P, N);
-            ShiftLeft(S, N);
-            loop--;
-        }
-        return max;
-    };
 
     void Run_Problem1_5(const char* inputPath)
     {
         FILE* fi = freopen(inputPath, "r", stdin);
         if (fi)
         {
-            INT32 T; //T ≤ 300
-            INT32 N;//1 ≤ N ≤ 2,000
-            UINT16 *P;//1 -> 1000
-            UINT16 *S;//0 or 1
-
-            cin >> T;
-            //printf("%d\n",T);
-            UINT64 *sum;
-            while(T--)
-            {
+            int i, t;
+            int max;
+            //scanf("%d", &t);
+            cin >> t;
+            while (t--) {
+                //scanf("%d", &N);
                 cin >> N;
-                //printf("%d\n", N);
-                P = new UINT16[N+1];
-                S = new UINT16[N + 1];
-                sum = new UINT64[N + 1];//sum[i] là tổng điện lớn nhất của 1 -> i snakes có thể tạo ra
-                P[0] = -1;
-                S[0] = -1;
-                for (int i = 1; i <= N; i++)
-                {
-                    cin >> P[i];
-                    //printf("%d ", P[i]);
+                memset(chk_cal, 0, sizeof(chk_cal));
+                memset(next_fe, 0, sizeof(next_fe));
+                for (i = 0; i < N; i++) {
+                    //scanf("%d", &power[i]);
+                    cin >> power[i];
                 }
-                //printf("\n");
-                for (int i = 1; i <= N; i++)
-                {
-                    cin >> S[i];
-                    //printf("%d ", S[i]);
+                for (i = 0; i < N; i++) {
+                    //scanf("%d", &female[i]);
+                    cin >> female[i];
                 }
-                //printf("\n");
-                //giai toan bang quy hoach dong
-                UINT64 max = getMaximumAmount(N, P, S, sum);
-                
-                delete[] P;
-                delete[] S;
-                delete[] sum;
-                cout << max << std::endl;
+                max = find_max_power();
+                printf("%d\n", max);
             }
 
             fclose(fi);
