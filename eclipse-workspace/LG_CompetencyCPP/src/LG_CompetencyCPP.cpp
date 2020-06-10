48fe7785ab2036b9d1d0697d6e5134738cf3c68a
minhthedt <minhthedt@gmail.com>
2020-06-10 22:30:08 +0700

    add eclipse-workspace

---
+++ eclipse-workspace/LG_CompetencyCPP/src/LG_CompetencyCPP.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : LG_CompetencyCPP.cpp
+// Author      : the.vu
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
